f3c1469ee8df11a2d0c647fbbbcb17a09bb323c2
Krzysztof <krzysztof_rrr@wp.pl>
2019-08-13 23:33:44 +0200

    Add one provisioning script for all openhab environments

---
+++ provision_openhab_vm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "please specify environment [dev|test|prod], eg: $0 dev"
+    exit
+fi
+
+case $1 in
+    prod) echo you are working on prod; echo Please check and press key to continue or ctrl-c to exit; read ;;
+    dev | test ) ;;
+    *) echo please specify correct parameters; exit 1;;
+esac
+
+
+ansible-playbook -i inventory.yml -e 'ansible_python_interpreter=/usr/bin/python3' openhab-$1.yml
